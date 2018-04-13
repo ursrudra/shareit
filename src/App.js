f22b45c90af6ce14cf06ec9f81993054b2a96e6e
Rudra Kishore <giduturirudrakishore@gmail.com>
2018-04-13 12:08:19 +0530

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col } from 'reactstrap';
+import Footer from './Components/Footer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container fluid={true}>
+      
+      <Footer/>
+      </Container>
     );
   }
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Footer = ( ) =>{
+	
+	return (
+                          <p>&copy; Rudra Kishore {new Date().getFullYear()}</p>
+		)
+}
+
+export default Footer
